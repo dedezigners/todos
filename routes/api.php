7948c0260514cd389e37e23e6242b01d1ad00035
Atif Ibrahim <dedezigners@gmail.com>
2020-08-24 11:59:46 +0500

    JWT Authentication

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
 Route::get('/questions', 'QuestionController@index')->name('question.index');
 Route::apiResource('/question', 'QuestionController')->except('index');
 
