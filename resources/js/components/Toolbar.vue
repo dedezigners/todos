3cdcf812a18493975b15efe65e2ed2a3470cc871
Atif Ibrahim <dedezigners@gmail.com>
2020-08-25 14:44:45 +0500

    Home Component vuetify

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>DeFourm</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text>Fourm</v-btn>
+    <v-btn text>Ask Question</v-btn>
+    <v-btn text>Category</v-btn>
+    <v-btn text>Login</v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,18 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Discussion Forum</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <example-component></example-component>
+        <v-app>
+            <app-home></app-home>
+        </v-app>
     </div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
