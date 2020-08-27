e35b2003dfe5b98cdc53bd2df2b84531f5911e32
Atif Ibrahim <dedezigners@gmail.com>
2020-08-27 17:37:32 +0500

    Exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,12 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +54,24 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof TokenBlacklistedException) {
+            return response()->json([
+                'message' => "Token blacklisted, get new one."
+            ], 400);
+        } elseif ($exception instanceof TokenExpiredException) {
+            return response()->json([
+                'message' => "Token expired."
+            ], 400);
+        } elseif ($exception instanceof TokenInvalidException) {
+            return response()->json([
+                'message' => "Token invalid."
+            ], 400);
+        } elseif ($exception instanceof JWTException) {
+            return response()->json([
+                'message' => "Token is not provided."
+            ], 400);
+        }
+
         return parent::render($request, $exception);
     }
 }
--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -9,6 +9,16 @@ use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
+    /**
+     * Create a new LikeController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('jwt');
+    }
+
     public function like(Reply $reply)
     {
         $reply->likes()->create([
